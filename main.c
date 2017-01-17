78b55483aaee19278e0179fd88d7cdff4d7ee4fb
Friestad <bryanf@iastate.edu>
2017-01-17 17:40:25 -0600

    2nd version

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ gcc main.c dot.c -o main
 
 void scanV(double v[], int num) {
 	int i;
-	for (i=0; i<num; i++) {
+	for (i = 0; i < num; i++) {
 		scanf(" %lf", &(v[i]));
 		}
 	}
@@ -28,8 +28,11 @@ int main(int argc, char* argv[]) {
 		printf("First command line argument must be an integer indicating vector lengths\n");
 		return -1;
 	}
-	v1 = (double*) malloc(vLen*sizeof(double));
-	v2 = (double*) malloc(vLen*sizeof(double));
+	else{
+		printf("%d\n", vLen);
+	}
+	v1 = (double*) malloc(vLen * sizeof(double));
+	v2 = (double*) malloc(vLen * sizeof(double));
 
 	printf("Enter a vector of %d numbers on one line separated by spaces\n", vLen);
 	scanV(v1, vLen);
@@ -38,8 +41,8 @@ int main(int argc, char* argv[]) {
 	scanV(v2, vLen);
 	
 	//need to print out the two vectors to make sure shit isn't fucked up at this point
-	printf("<%lf, %lf>\n", v1[0], v1[1]); //**RETURNS <2.000000, 3.000000>**
-	printf("<%lf, %lf>\n", v2[0], v2[1]); //**RETURNS <4.000000, 1.000000>**
+	//printf("<%lf, %lf, %lf, %lf>\n", v1[0], v1[1], v1[2], v1[3]); //**RETURNS <2.000000, 3.000000>**
+	//printf("<%lf, %lf, %lf, %lf>\n", v2[0], v2[1], v2[2], v2[3]); //**RETURNS <4.000000, 1.000000>**
 	//from doing this i found out that the problem is not with scanning in the numbers.
 
 	dot1 = dot(v1, v2, vLen);
